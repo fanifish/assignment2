b9d310880019270fedba87e42c0f1843c2b51621
fanifanu <fanifanu@gmail.com>
2015-02-10 00:43:36 -0600

    question 1 for hw

---
+++ CyclicBarrier.java
@@ -0,0 +1,16 @@
+/*
+*   @ author faniel ghirmay
+*/
+public class CyclicBarrier{
+	int numOfThreads;
+	public CyclicBarrier(int parties){
+		numOfThreads = parties;
+	}
+	/**
+	THis holds the threads until all of them make the call
+	*/
+	public int await(){
+		int index;
+		
+	}
+}
