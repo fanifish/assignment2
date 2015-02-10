b78462e809919becef55b2db82a734a49f0fd5db
fanifanu <fanifanu@gmail.com>
2015-02-10 01:05:24 -0600

    some modification

--- CyclicBarrier.java
+++ CyclicBarrier.java
@@ -1,16 +1,24 @@
-/*
-*   @ author faniel ghirmay
-*/
-public class CyclicBarrier{
+/**
+ *  synchronization aid that allows a set of threads to wait for each other
+ *  @author faniel ghirmay ffg92 & jode garcia jag7235
+ *
+ */
+public class CyclicBarrier {
 	int numOfThreads;
+	
 	public CyclicBarrier(int parties){
 		numOfThreads = parties;
 	}
 	/**
-	THis holds the threads until all of them make the call
-	*/
-	public int await(){
-		int index;
+	 * waits until all the threads enter the wait queue before releasing them 
+	 * @return
+	 * @throws InterruptedException
+	 */
+	int await() throws InterruptedException{
+		while(numOfThreads != 0)
+			Util.myWait();
+		return 0;
 		
 	}
 }
+
