97639c0933ec37e38a1f3fefd7588af0793c0b3d
fanifish <fanifish@users.noreply.github.com>
2015-02-11 01:00:26 -0600

    Update CyclicBarrier.java
    some trial

--- CyclicBarrier.java
+++ CyclicBarrier.java
@@ -1,24 +1,87 @@
-/**
- *  synchronization aid that allows a set of threads to wait for each other
- *  @author faniel ghirmay ffg92 & jode garcia jag7235
- *
- */
+package com.utexas.fani;
+
+import java.util.concurrent.Semaphore;
+import java.lang.Object;
+import java.util.Objects;
+import java.util.concurrent.locks.*;
+import java.util.function.*;
+
 public class CyclicBarrier {
 	int numOfThreads;
-	
-	public CyclicBarrier(int parties){
+	int parties;
+	int arrivalIndex;
+	int x;
+	BinarySemaphore mutex;
+	CountingSemaphore sync;
+
+	public CyclicBarrier(int parties) {
 		numOfThreads = parties;
+		this.parties = parties;
+		mutex = new BinarySemaphore(true); // guarantees first in first out
+		sync = new CountingSemaphore(parties);
 	}
+
 	/**
-	 * waits until all the threads enter the wait queue before releasing them 
+	 * waits until all threads have invoked await
+	 * 
 	 * @return
 	 * @throws InterruptedException
 	 */
-	int await() throws InterruptedException{
-		while(numOfThreads != 0)
-			Util.myWait();
-		return 0;
-		
+	int await() throws InterruptedException {
+		System.out.println("num of threads calling " + numOfThreads);
+		int arrivalIndex = 0;
+		mutex.P();
+		numOfThreads--;
+		arrivalIndex = numOfThreads;
+		mutex.V();
+		sync.P();
+		sync.V();
+		numOfThreads = parties;
+		sync = new CountingSemaphore(parties);
+		return arrivalIndex;
 	}
+
+	public static void main(String args[]) throws InterruptedException {
+		CyclicBarrier sync = new CyclicBarrier(5);
+		Thread t[] = new Thread[5];
+		for (int i = 0; i < 5; i += 1) {
+			t[i] = new Thread(new po(i, sync));
+			t[i].start();
+		}
+		
+		System.out.println("Next phase");
+
+		for (int i = 0; i < 1000000000; i += 1) {
+			
+		}
+
+		System.out.println("all released");
+
+	}
+
 }
+
+class po implements Runnable {
+	int x;
+    CyclicBarrier w;
+	po(int n, CyclicBarrier wb) {
+		x = n;
+		w = wb;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("This is thread num " + x);
+		try {
+			System.out.println("The barries release num " + w.await());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+
+
 
